b055bf022d886570f6cb26c3715cc0bf9e70e673
David Hu <hujinxin0607@gmail.com>
2022-08-16 22:36:28 -0700

    Built UserProfileWrite component
    Built UserProfileWrite component allow user post a post

---
+++ src/components/profile/UserProfileWrite.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="card edit-field">
+    <div class="card-body">
+      <label for="edit-post" class="form-label">Edit Post</label>
+      <textarea
+        v-model="content"
+        class="form-control"
+        id="edit-post"
+        rows="3"
+      ></textarea>
+      <button
+        @click="post_a_post"
+        type="button"
+        class="btn btn-primary btn-sm edit-submit"
+      >
+        Submit
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  name: "UserProfileWrite",
+  setup(props, context) {
+    let content = ref("");
+
+    const post_a_post = () => {
+      context.emit("post_a_post", content.value);
+      content.value = "";
+    };
+    return {
+      content,
+      post_a_post,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.edit-field {
+  margin-top: 20px;
+}
+.edit-submit {
+  margin-top: 10px;
+}
+</style>
