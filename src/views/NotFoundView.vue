5dc4334054b5c9342f3555fa4af272c743b87b62
David Hu <hujinxin0607@gmail.com>
2022-08-16 22:38:09 -0700

    Update 404 pageview
    Update 404 pageview add a fashion picture into page.

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,5 +1,10 @@
 <template>
-  <ContentBase> 404 </ContentBase>
+  <ContentBase>
+    <img
+      src="https://cdn.mos.cms.futurecdn.net/PuXipAW3AXUzUJ4uYyxPKC-1200-80.jpg"
+      alt="404"
+    />
+  </ContentBase>
 </template>
 
 <script>
