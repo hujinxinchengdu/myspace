9040363df0c366a4d1fff2c365a5a6e8b01c14ca
David Hu <hujinxin0607@gmail.com>
2022-08-17 00:32:59 -0700

    finish login component

--- src/store/user.js
+++ src/store/user.js
@@ -7,10 +7,25 @@ const ModuleUser = {
         username: "",
         photo: "",
         followerCount: 0,
-    },
+        access: "",
+        refresh: "",
+        is_login: false,
+      },
     getters: {
     },
     mutations: {
+        updateUser(state, user) {
+            state.id = user.id;
+            state.username = user.username;
+            state.photo = user.photo;
+            state.followerCount = user.followerCount;
+            state.access = user.access;
+            state.refresh = user.refresh;
+            state.is_login = user.is_login;
+        },
+        updateAccess(state, access) {
+            state.access = access;
+        },
     },
     actions: {
         login(context, data){
@@ -24,6 +39,20 @@ const ModuleUser = {
                 success(resp) {
                     const {access, refresh} = resp;
                     const access_obj = jwt_decode(access);
+
+                    setInterval(() => {
+                        $.ajax({
+                            url: "https://app165.acapp.acwing.com.cn/api/token/refresh/",
+                            type: "POST",
+                            data: {
+                                refresh,
+                            },
+                            success(resp) {
+                                context.commit('updateAccess', resp.access);
+                            }
+                        });
+                    }, 4.5 * 60 * 1000);
+
                     $.ajax({
                         url: "https://app165.acapp.acwing.com.cn/myspace/getinfo/",
                         type: "GET",
@@ -34,9 +63,18 @@ const ModuleUser = {
                             'Authorization': "Bearer " + access,
                         },
                         success(resp) {
-                            console.log(resp, refresh);
+                            context.commit("updateUser", {
+                                ...resp,
+                                access: access,
+                                refresh: refresh,
+                                is_login: true,
+                            });
+                            data.success();
                         },
                     });
+                },
+                error() {
+                    data.error();
                 }
             });
         }
