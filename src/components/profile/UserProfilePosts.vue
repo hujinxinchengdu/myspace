ad70fbede2a1ec7c7c727335d1ebbc63ce9471aa
David Hu <hujinxin0607@gmail.com>
2022-08-17 11:15:51 -0700

    Update UserProfilePosts.vue
    add delete post button and implement it base on api

--- src/components/profile/UserProfilePosts.vue
+++ src/components/profile/UserProfilePosts.vue
@@ -5,6 +5,14 @@
         <div class="card single-post">
           <div class="card-body">
             {{ post.content }}
+            <button
+              @click="delete_a_post(post.id)"
+              v-if="is_me"
+              type="button"
+              class="btn btn-danger btn-sm"
+            >
+              Delete
+            </button>
           </div>
         </div>
       </div>
@@ -13,6 +21,10 @@
 </template>
 
 <script>
+import { computed } from "vue";
+import { useStore } from "vuex";
+import $ from "jquery";
+
 export default {
   name: "UserProfilePosts",
   props: {
@@ -20,6 +32,39 @@ export default {
       type: Object,
       required: true,
     },
+    user: {
+      type: Object,
+      required: true,
+    },
+  },
+  setup(props, context) {
+    const store = useStore();
+    let is_me = computed(() => {
+      return props.user.id === store.state.user.id;
+    });
+
+    const delete_a_post = (post_id) => {
+      $.ajax({
+        url: "https://app165.acapp.acwing.com.cn/myspace/post/",
+        type: "DELETE",
+        data: {
+          post_id: post_id,
+        },
+        headers: {
+          Authorization: "Bearer " + store.state.user.access,
+        },
+        success(resp) {
+          if (resp.result === "success") {
+            context.emit("delete_a_post", post_id);
+          }
+        },
+      });
+    };
+
+    return {
+      is_me,
+      delete_a_post,
+    };
   },
 };
 </script>
@@ -27,5 +72,10 @@ export default {
 <style scoped>
 .single-post {
   margin-bottom: 10px;
+}
+
+button {
+  float: right;
+  font-size: 10px;
 }
 </style>
